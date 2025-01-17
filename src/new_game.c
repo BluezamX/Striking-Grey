ac0de86cb2a0115b47d4b6e9e97866a86a5853a4
Jasper Wijnhoven <jasper.wijnhoven@hotmail.com>
2025-01-17 17:30:15 +0100

    Remove Truck sequence

--- src/new_game.c
+++ src/new_game.c
@@ -45,6 +45,7 @@
 #include "mystery_gift.h"
 #include "union_room_chat.h"
 #include "constants/items.h"
+#include "maps.h"
 
 extern const u8 EventScript_ResetAllMapFlags[];
 
@@ -127,7 +128,7 @@ static void ClearFrontierRecord(void)
 
 static void WarpToTruck(void)
 {
-    SetWarpDestination(MAP_GROUP(INSIDE_OF_TRUCK), MAP_NUM(INSIDE_OF_TRUCK), WARP_ID_NONE, -1, -1);
+    SetWarpDestination(MAP_GROUP(NUVEMA_TOWN), MAP_NUM(NUVEMA_TOWN), WARP_ID_NONE, -1, -1);
     WarpIntoMap();
 }
 
